785639006cc1fb5168d40c2ae7906b086b2f93b6
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2013-10-27 13:55:32 +0100

    Improve drink model with ingredients, instructions and history

--- Drinks/src/main/java/fr/masciulli/drinks/model/Drink.java
+++ Drinks/src/main/java/fr/masciulli/drinks/model/Drink.java
@@ -1,8 +1,14 @@
 package fr.masciulli.drinks.model;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Drink {
     private String mName;
     private String mImageURL;
+    private String mHistory;
+    private List<String> mIngredients = new LinkedList<String>();
+    private List<String> mInstructions = new LinkedList<String>();
 
     public Drink(String name, String imageURL) {
         mName = name;
@@ -23,5 +29,29 @@ public class Drink {
 
     public void setImageURL(String imageURL) {
         mImageURL = imageURL;
+    }
+
+    public String getHistory() {
+        return mHistory;
+    }
+
+    public void setHistory(String history) {
+        mHistory = history;
+    }
+
+    public void addIngredient(String ingredient) {
+        mIngredients.add(ingredient);
+    }
+
+    public List<String> getIngredients() {
+        return mIngredients;
+    }
+
+    public void addIntruction(String instruction) {
+        mInstructions.add(instruction);
+    }
+
+    public List<String> getInstructions() {
+        return mInstructions;
     }
 }
