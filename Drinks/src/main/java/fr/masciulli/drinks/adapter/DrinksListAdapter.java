1a80f518c05bf86817edfd2ecf3ed185b18b496a
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2013-10-30 17:32:20 +0100

    Change Tom Collins url to visualize effect

--- Drinks/src/main/java/fr/masciulli/drinks/adapter/DrinksListAdapter.java
+++ Drinks/src/main/java/fr/masciulli/drinks/adapter/DrinksListAdapter.java
@@ -28,7 +28,7 @@ public class DrinksListAdapter extends BaseAdapter {
         for (int i = 0; i < 100; i++) {
             mDrinks.add(new Drink("Amaretto Frost", "http://www.smallscreennetwork.com/videos/cocktail_spirit/morgenthaler-method-amaretto-sour.jpg"));
             mDrinks.add(new Drink("Americano", "http://www.ganzomag.com/wp-content/uploads/2012/05/americano-cocktail1.jpg"));
-            mDrinks.add(new Drink("Tom Collins", "http://www.theginblog.co.uk/home/wp-content/uploads/tom-collins-drink.jpg"));
+            mDrinks.add(new Drink("Tom Collins", "http://www.vinumimporting.com/wp-content/uploads/2012/06/tom-collins.jpg"));
             mDrinks.add(new Drink("Mojito", "http://2eat2drink.files.wordpress.com/2011/04/mojito-final2.jpg"));
             mDrinks.add(new Drink("Dry Martini", "http://www.cocktailrendezvous.com/images.php?f=files/recipes/images/martini.jpg&w=616&h=347&c=1"));
         }
