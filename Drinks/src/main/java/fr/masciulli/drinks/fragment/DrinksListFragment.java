d446ffec63bcb688fcf36ea69b79252f1334d68a
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2013-11-10 14:24:50 +0100

    Add some Croutons in list

--- Drinks/src/main/java/fr/masciulli/drinks/fragment/DrinksListFragment.java
+++ Drinks/src/main/java/fr/masciulli/drinks/fragment/DrinksListFragment.java
@@ -12,11 +12,14 @@ import android.widget.ListView;
 
 import java.util.List;
 
+import de.keyboardsurfer.android.widget.crouton.Crouton;
+import de.keyboardsurfer.android.widget.crouton.Style;
 import fr.masciulli.drinks.activity.DrinkDetailActivity;
 import fr.masciulli.drinks.adapter.DrinksListAdapter;
 import fr.masciulli.drinks.R;
 import fr.masciulli.drinks.data.DrinksProvider;
 import fr.masciulli.drinks.model.DrinksListItem;
+import fr.masciulli.drinks.util.ConnectionUtils;
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
@@ -33,7 +36,12 @@ public class DrinksListFragment extends Fragment implements AdapterView.OnItemCl
         mListView.setOnItemClickListener(this);
         mListAdapter = new DrinksListAdapter(getActivity());
         mListView.setAdapter(mListAdapter);
-        DrinksProvider.getDrinksList(this);
+        if (ConnectionUtils.isOnline(getActivity())) {
+            DrinksProvider.getDrinksList(this);
+        } else {
+            Crouton.makeText(getActivity(), getString(R.string.network_error), Style.ALERT).show();
+        }
+
         return root;
     }
 
@@ -57,5 +65,6 @@ public class DrinksListFragment extends Fragment implements AdapterView.OnItemCl
     @Override
     public void failure(RetrofitError retrofitError) {
         Log.e(this.getClass().getName(), "Drinks list loading has failed");
+        Crouton.makeText(getActivity(), getString(R.string.list_loading_failed), Style.ALERT).show();
     }
 }
