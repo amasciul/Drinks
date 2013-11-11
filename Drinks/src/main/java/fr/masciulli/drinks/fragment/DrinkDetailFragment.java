cf5199169e33231b40577439bb7dbc5469095d64
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2013-11-11 12:24:59 +0100

    Blurred image alpha at 1 when image half scrolled

--- Drinks/src/main/java/fr/masciulli/drinks/fragment/DrinkDetailFragment.java
+++ Drinks/src/main/java/fr/masciulli/drinks/fragment/DrinkDetailFragment.java
@@ -93,7 +93,7 @@ public class DrinkDetailFragment extends Fragment implements ScrollViewListener,
     @Override
     public void onScrollChanged(ObservableScrollView scrollView, int x, int y, int oldx, int oldy) {
         //TODO better alpha calculation (blur not visible enough)
-        float alpha = (float) y / (float) mImageViewHeight;
+        float alpha = 2 * (float) y / (float) mImageViewHeight;
         if (alpha > 1) {
             alpha = 1;
         }
