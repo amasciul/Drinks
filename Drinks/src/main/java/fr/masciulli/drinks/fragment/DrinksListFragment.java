6ed94bf2675b84527913540fc4908285db8c7c82
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2013-11-14 11:27:40 +0100

    Add progress bar and retry button on list

--- Drinks/src/main/java/fr/masciulli/drinks/fragment/DrinksListFragment.java
+++ Drinks/src/main/java/fr/masciulli/drinks/fragment/DrinksListFragment.java
@@ -8,7 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 
 import java.util.List;
 
@@ -24,25 +26,36 @@ import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 
-public class DrinksListFragment extends Fragment implements AdapterView.OnItemClickListener, Callback<List<DrinksListItem>> {
+public class DrinksListFragment extends Fragment implements AdapterView.OnItemClickListener, Callback<List<DrinksListItem>>, View.OnClickListener {
     private ListView mListView;
+    private ProgressBar mProgressBar;
+
     private DrinksListAdapter mListAdapter;
+    private Button mRefreshButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View root = inflater.inflate(R.layout.fragment_drinks, container, false);
 
         mListView = (ListView) root.findViewById(R.id.list);
+        mProgressBar = (ProgressBar)root.findViewById(R.id.progressbar);
+        mRefreshButton = (Button)root.findViewById(R.id.refresh);
+
+        mRefreshButton.setOnClickListener(this);
+
         mListView.setOnItemClickListener(this);
         mListAdapter = new DrinksListAdapter(getActivity());
         mListView.setAdapter(mListAdapter);
-        if (ConnectionUtils.isOnline(getActivity())) {
-            DrinksProvider.getDrinksList(this);
-        } else {
-            Crouton.makeText(getActivity(), getString(R.string.network_error), Style.ALERT).show();
-        }
+
+        load();
 
         return root;
+    }
+
+    private void load() {
+        mProgressBar.setVisibility(View.VISIBLE);
+        mRefreshButton.setVisibility(View.GONE);
+        DrinksProvider.getDrinksList(this);
     }
 
     @Override
@@ -59,12 +72,27 @@ public class DrinksListFragment extends Fragment implements AdapterView.OnItemCl
     @Override
     public void success(List<DrinksListItem> drinks, Response response) {
         Log.d(this.getClass().getName(), "Drinks list loading has succeeded");
+        mListView.setVisibility(View.VISIBLE);
+        mProgressBar.setVisibility(View.GONE);
         mListAdapter.update(drinks);
     }
 
     @Override
     public void failure(RetrofitError retrofitError) {
+        mProgressBar.setVisibility(View.GONE);
+        mRefreshButton.setVisibility(View.VISIBLE);
         Log.e(this.getClass().getName(), "Drinks list loading has failed");
-        if (isAdded()) Crouton.makeText(getActivity(), getString(R.string.list_loading_failed), Style.ALERT).show();
+        if (isAdded()) {
+            if (retrofitError.isNetworkError()) {
+                Crouton.makeText(getActivity(), getString(R.string.network_error), Style.ALERT).show();
+            } else {
+                Crouton.makeText(getActivity(), getString(R.string.list_loading_failed), Style.ALERT).show();
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        load();
     }
 }
