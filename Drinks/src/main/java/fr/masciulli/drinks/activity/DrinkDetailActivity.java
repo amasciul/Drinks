3e941dd3b49f5c10a9160d89da45c81f05177470
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2013-12-15 17:13:47 +0100

    Fix crash on change orientation

--- Drinks/src/main/java/fr/masciulli/drinks/activity/DrinkDetailActivity.java
+++ Drinks/src/main/java/fr/masciulli/drinks/activity/DrinkDetailActivity.java
@@ -2,7 +2,6 @@ package fr.masciulli.drinks.activity;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.MenuItem;
 
 import fr.masciulli.drinks.R;
 import fr.masciulli.drinks.fragment.DrinkDetailFragment;
@@ -21,6 +20,8 @@ public class DrinkDetailActivity extends FragmentActivity {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.drink_detail_container, mDetailFragment)
                     .commit();
+        } else {
+            mDetailFragment = (DrinkDetailFragment) getSupportFragmentManager().findFragmentById(R.id.drink_detail_container);
         }
 
         getActionBar().setDisplayHomeAsUpEnabled(true);
