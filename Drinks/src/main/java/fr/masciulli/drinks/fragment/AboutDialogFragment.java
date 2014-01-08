4e4eca4cc9445c332eaaee5d90e10d148b79ce84
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2014-01-08 19:00:32 +0100

    Add version number in about dialog

--- Drinks/src/main/java/fr/masciulli/drinks/fragment/AboutDialogFragment.java
+++ Drinks/src/main/java/fr/masciulli/drinks/fragment/AboutDialogFragment.java
@@ -2,9 +2,13 @@ package fr.masciulli.drinks.fragment;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 import fr.masciulli.drinks.R;
 
@@ -14,10 +18,22 @@ public class AboutDialogFragment extends DialogFragment {
 
         LayoutInflater inflater = getActivity().getLayoutInflater();
 
+        View root = inflater.inflate(R.layout.dialog_about, null);
+
+        TextView versionNameView = (TextView) root.findViewById(R.id.version_name);
+
+        try {
+            PackageInfo pInfo = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0);
+            versionNameView.setText(pInfo.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            versionNameView.setText(getString(R.string.unknown_version));
+        }
+
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder
                 .setPositiveButton(getString(R.string.about_ok), null)
-                .setView(inflater.inflate(R.layout.dialog_about, null));
+                .setView(root);
 
         return builder.create();
     }
