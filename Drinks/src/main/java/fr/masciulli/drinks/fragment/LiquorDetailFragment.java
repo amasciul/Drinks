6cc69021dc9ec924dbd8c7d8df12fa6c16c53168
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2014-03-28 09:24:39 +0100

    Launch liquor detail loading on image animation start

--- Drinks/src/main/java/fr/masciulli/drinks/fragment/LiquorDetailFragment.java
+++ Drinks/src/main/java/fr/masciulli/drinks/fragment/LiquorDetailFragment.java
@@ -302,12 +302,12 @@ public class LiquorDetailFragment extends Fragment implements Callback<Liquor>, 
 
         mImageView.setTranslationY(-mImageView.getHeight());
 
-        ViewPropertyAnimator animator = mImageView.animate().setDuration(ANIM_IMAGE_ENTER_DURATION).
+        mImageView.animate().setDuration(ANIM_IMAGE_ENTER_DURATION).
                 setStartDelay(ANIM_IMAGE_ENTER_STARTDELAY).
+                //TODO handle API < 16
+                withStartAction(refreshRunnable).
                 translationY(0).
                 setInterpolator(sDecelerator);
-
-        AnimUtils.scheduleEndAction(animator, refreshRunnable, ANIM_IMAGE_ENTER_DURATION);
 
         ObjectAnimator bgAnim = ObjectAnimator.ofInt(mBackground, "alpha", 0, 255);
         bgAnim.setDuration(ANIM_IMAGE_ENTER_DURATION);
