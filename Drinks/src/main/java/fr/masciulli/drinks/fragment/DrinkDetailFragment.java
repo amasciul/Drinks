ab0712b18ae5cd2a19ff2794b144927545fe3fa5
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2014-04-17 09:37:52 +0200

    Remove useless imports

--- Drinks/src/main/java/fr/masciulli/drinks/fragment/DrinkDetailFragment.java
+++ Drinks/src/main/java/fr/masciulli/drinks/fragment/DrinkDetailFragment.java
@@ -10,8 +10,6 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Html;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
