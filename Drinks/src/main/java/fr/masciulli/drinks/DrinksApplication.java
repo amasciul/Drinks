f2083925948201b4d49b6fc916f9c3de464dec6b
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2014-04-27 16:27:05 +0200

    Save version code to detect updates

--- Drinks/src/main/java/fr/masciulli/drinks/DrinksApplication.java
+++ Drinks/src/main/java/fr/masciulli/drinks/DrinksApplication.java
@@ -1,7 +1,11 @@
 package fr.masciulli.drinks;
 
 import android.app.Application;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 import fr.masciulli.drinks.data.DrinksProvider;
 
@@ -10,8 +14,28 @@ public class DrinksApplication extends Application {
     public void onCreate() {
         super.onCreate();
         String apiPrefKey = getString(R.string.pref_apiendpoint_key);
+        String versionCodePrefKey = getString(R.string.pref_versioncode_key);
         String apiDefaultValue = getString(R.string.pref_apiendpoint_default);
-        String server = PreferenceManager.getDefaultSharedPreferences(this).getString(apiPrefKey, apiDefaultValue);
+
+        String server;
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            int versionCode = packageInfo.versionCode;
+            int lastVersionCode = PreferenceManager.getDefaultSharedPreferences(this).getInt(versionCodePrefKey, 0);
+            if (lastVersionCode < versionCode) {
+                // Update detected
+                server = apiDefaultValue;
+                SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(this).edit();
+                editor.putInt(versionCodePrefKey, versionCode);
+                editor.putString(apiPrefKey, apiDefaultValue);
+                editor.commit();
+            } else {
+                server = PreferenceManager.getDefaultSharedPreferences(this).getString(apiPrefKey, apiDefaultValue);
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            server = apiDefaultValue;
+        }
+
         DrinksProvider.updateServer(server);
     }
 }
