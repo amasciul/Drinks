1315ecebffa8e2daf7b427f17c5af6ff9c240910
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2014-11-23 18:25:23 +0100

    Fix settings activity

--- Drinks/src/main/java/fr/masciulli/drinks/activity/SettingsActivity.java
+++ Drinks/src/main/java/fr/masciulli/drinks/activity/SettingsActivity.java
@@ -4,19 +4,25 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
 
+import fr.masciulli.drinks.R;
 import fr.masciulli.drinks.fragment.SettingsFragment;
 
-public class SettingsActivity extends ActionBarActivity {
+public class SettingsActivity extends ToolbarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
+                .replace(R.id.content, new SettingsFragment())
                 .commit();
 
         getSupportActionBar()
                 .setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_settings;
     }
 
     @Override
