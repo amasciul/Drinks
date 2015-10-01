f5401e30026c2a538bda87a7b324126629a20bd5
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2015-10-01 21:58:28 +0200

    Complete drink model

--- app/src/main/java/fr/masciulli/drinks/model/Drink.java
+++ app/src/main/java/fr/masciulli/drinks/model/Drink.java
@@ -1,5 +1,11 @@
 package fr.masciulli.drinks.model;
 
+import java.util.List;
+
 public class Drink {
     public String name;
+    public String imageUrl;
+    public String wikipedia;
+    public String instructions;
+    public List<String> ingredients;
 }
