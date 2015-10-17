9773dd06cfd03c215f4949230148017decccc8d7
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2015-10-17 19:13:57 +0200

    Remove unused imports

--- app/src/main/java/fr/masciulli/drinks/ui/DrinksFragment.java
+++ app/src/main/java/fr/masciulli/drinks/ui/DrinksFragment.java
@@ -2,8 +2,6 @@ package fr.masciulli.drinks.ui;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
