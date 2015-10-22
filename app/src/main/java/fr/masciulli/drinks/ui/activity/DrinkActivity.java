04d8de2c01d2dc5e87eb59f5b2873481c96ee25e
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2015-10-22 15:59:08 +0200

    Add ingredients and instructions

--- app/src/main/java/fr/masciulli/drinks/ui/activity/DrinkActivity.java
+++ app/src/main/java/fr/masciulli/drinks/ui/activity/DrinkActivity.java
@@ -3,6 +3,8 @@ package fr.masciulli.drinks.ui.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.Spanned;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,11 +17,12 @@ public class DrinkActivity extends AppCompatActivity {
     private static final String TAG = DrinkActivity.class.getSimpleName();
 
     public static final String EXTRA_DRINK = "extra_drink";
+    private Drink drink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Drink drink = getIntent().getParcelableExtra(EXTRA_DRINK);
+        drink = getIntent().getParcelableExtra(EXTRA_DRINK);
         setContentView(R.layout.activity_drink);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -29,8 +32,25 @@ public class DrinkActivity extends AppCompatActivity {
 
         ImageView imageView = (ImageView) findViewById(R.id.image);
         TextView historyView = (TextView) findViewById(R.id.history);
+        TextView instructionsView = (TextView) findViewById(R.id.instructions);
+        TextView ingredientsView = (TextView) findViewById(R.id.ingredients);
 
         Picasso.with(this).load(drink.imageUrl).into(imageView);
         historyView.setText(drink.history);
+        instructionsView.setText(drink.instructions);
+        ingredientsView.setText(parseIngredients());
+    }
+
+    private Spanned parseIngredients() {
+        StringBuilder builder = new StringBuilder();
+        int i = 0;
+        for (String ingredient : drink.ingredients) {
+            builder.append("&#8226; ");
+            builder.append(ingredient);
+            if (++i < drink.ingredients.size()) {
+                builder.append("<br>");
+            }
+        }
+        return Html.fromHtml(builder.toString());
     }
 }
