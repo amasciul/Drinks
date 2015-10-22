d756fcb5e1740d562fd2210fa762927fe1670a09
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2015-10-22 15:16:33 +0200

    Add collapsing toolbar layout on drink detail

--- app/src/main/java/fr/masciulli/drinks/ui/activity/DrinkActivity.java
+++ app/src/main/java/fr/masciulli/drinks/ui/activity/DrinkActivity.java
@@ -2,9 +2,8 @@ package fr.masciulli.drinks.ui.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.ViewTreeObserver;
+import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
@@ -17,33 +16,21 @@ public class DrinkActivity extends AppCompatActivity {
 
     public static final String EXTRA_DRINK = "extra_drink";
 
-    private TextView historyView;
-    private ImageView imageView;
-    private TextView nameView;
-    private ScrollView scrollView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Drink drink = getIntent().getParcelableExtra(EXTRA_DRINK);
         setContentView(R.layout.activity_drink);
 
-        imageView = (ImageView) findViewById(R.id.image);
-        historyView = (TextView) findViewById(R.id.history);
-        nameView = (TextView) findViewById(R.id.name);
-        scrollView = (ScrollView) findViewById(R.id.scroll);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setTitle(drink.name);
+
+        ImageView imageView = (ImageView) findViewById(R.id.image);
+        TextView historyView = (TextView) findViewById(R.id.history);
 
         Picasso.with(this).load(drink.imageUrl).into(imageView);
         historyView.setText(drink.history);
-        nameView.setText(drink.name);
-
-        scrollView.getViewTreeObserver()
-                .addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
-                    @Override
-                    public void onScrollChanged() {
-                        int y = scrollView.getScrollY();
-                        imageView.setTop(-y / 2);
-                    }
-                });
     }
 }
