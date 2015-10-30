0a81b8169b5d99108dbb07cdfb24342a5e934378
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2015-10-30 21:58:19 +0100

    Add liquor images

--- app/src/main/java/fr/masciulli/drinks/model/Liquor.java
+++ app/src/main/java/fr/masciulli/drinks/model/Liquor.java
@@ -2,8 +2,22 @@ package fr.masciulli.drinks.model;
 
 public class Liquor {
     private String name;
+    private String imageUrl;
+    private float ratio;
 
     public String getName() {
         return name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setRatio(float ratio) {
+        this.ratio = ratio;
+    }
+
+    public float getRatio() {
+        return ratio;
     }
 }
--- app/src/main/java/fr/masciulli/drinks/ui/adapter/LiquorsAdapter.java
+++ app/src/main/java/fr/masciulli/drinks/ui/adapter/LiquorsAdapter.java
@@ -6,16 +6,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import fr.masciulli.drinks.R;
 import fr.masciulli.drinks.model.Liquor;
+import fr.masciulli.drinks.ui.view.RatioImageView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class LiquorsAdapter extends RecyclerView.Adapter<LiquorsAdapter.ViewHolder> {
+    private static float[] ratios = new float[]{0.75f, 4.0f / 3.0f};
 
     private ItemClickListener<Liquor> listener;
-    private List<Liquor> liquors = new ArrayList<>();
+    private ArrayList<Liquor> liquors = new ArrayList<>();
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -28,6 +32,11 @@ public class LiquorsAdapter extends RecyclerView.Adapter<LiquorsAdapter.ViewHold
     public void onBindViewHolder(ViewHolder holder, final int position) {
         final Liquor liquor = liquors.get(position);
         holder.nameView.setText(liquor.getName());
+        holder.imageView.setRatio(liquor.getRatio());
+        Picasso.with(holder.itemView.getContext())
+                .load(liquor.getImageUrl())
+                .into(holder.imageView);
+
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,15 +57,28 @@ public class LiquorsAdapter extends RecyclerView.Adapter<LiquorsAdapter.ViewHold
     }
 
     public void setLiquors(List<Liquor> liquors) {
-        this.liquors = liquors;
+        this.liquors.clear();
+        this.liquors.addAll(liquors);
+        fakeRatios();
+        notifyDataSetChanged();
+    }
+
+    private void fakeRatios() {
+        // TODO remove this and use ratios given by server
+        for (int i = 0, size = liquors.size(); i < size; i++) {
+            Liquor liquor = liquors.get(i);
+            liquor.setRatio(ratios[i % ratios.length]);
+        }
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         private final TextView nameView;
+        private final RatioImageView imageView;
 
         public ViewHolder(View itemView) {
             super(itemView);
             nameView = (TextView) itemView.findViewById(R.id.name);
+            imageView = (RatioImageView) itemView.findViewById(R.id.image);
         }
     }
 }
