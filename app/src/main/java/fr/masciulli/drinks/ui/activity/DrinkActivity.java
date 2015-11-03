118d34e9cd09e6a84bf024f2f714cb1f7045eccb
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2015-11-03 13:22:41 +0100

    Remove unused import

--- app/src/main/java/fr/masciulli/drinks/ui/activity/DrinkActivity.java
+++ app/src/main/java/fr/masciulli/drinks/ui/activity/DrinkActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.text.Spanned;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
