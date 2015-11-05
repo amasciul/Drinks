a0978ff00577a35acadadb92d98c50bae2a83875
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2015-11-05 22:28:40 +0100

    Add about dialog

--- app/src/main/java/fr/masciulli/drinks/ui/activity/AboutActivity.java
+++ app/src/main/java/fr/masciulli/drinks/ui/activity/AboutActivity.java
@@ -2,6 +2,7 @@ package fr.masciulli.drinks.ui.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 import android.widget.TextView;
 
 import fr.masciulli.drinks.BuildConfig;
@@ -11,9 +12,11 @@ public class AboutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_about);
 
         TextView versionView = (TextView) findViewById(R.id.version);
-        versionView.setText(BuildConfig.VERSION_NAME);
+        versionView.setText(getString(R.string.about_version, BuildConfig.VERSION_NAME));
     }
 }
