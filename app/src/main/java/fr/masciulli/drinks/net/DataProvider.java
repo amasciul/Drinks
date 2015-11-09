2fd6ff28339789554cb65083ea34ce52855175c0
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2015-11-09 19:04:56 +0100

    Extract cache control constants

--- app/src/main/java/fr/masciulli/drinks/net/DataProvider.java
+++ app/src/main/java/fr/masciulli/drinks/net/DataProvider.java
@@ -24,6 +24,11 @@ public class DataProvider {
     private static final String SERVER_BASE_URL = "http://drinks-api.appspot.com";
     private static final long CACHE_MAX_SIZE = 10 * 1024 * 1024;
 
+    private static final String HEADER_CACHE_CONTROL = "Cache-Control";
+    private static final String HEADER_CACHE_CONTROL_PUBLIC = "public";
+    private static final String HEADER_CACHE_CONTROL_ONLY_IF_CACHED = "public, only-if-cached, max-stale=";
+    private static final int HEADER_CACHE_CONTROL_MAX_STATE = 60 * 60 * 24 * 28;
+
     private WebApi retrofit;
 
     public DataProvider(final Context context) {
@@ -37,11 +42,10 @@ public class DataProvider {
             public Response intercept(Chain chain) throws IOException {
                 Request.Builder builder = chain.request().newBuilder();
                 if (new ConnectivityChecker(context).isConnectedOrConnecting()) {
-                    builder.addHeader("Cache-Control", "public");
+                    builder.addHeader(HEADER_CACHE_CONTROL, HEADER_CACHE_CONTROL_PUBLIC);
                 } else {
-                    int maxStale = 60 * 60 * 24 * 28;
-                    builder.addHeader("Cache-Control",
-                            "public, only-if-cached, max-stale=" + maxStale);
+                    builder.addHeader(HEADER_CACHE_CONTROL,
+                            HEADER_CACHE_CONTROL_ONLY_IF_CACHED + HEADER_CACHE_CONTROL_MAX_STATE);
                 }
                 return chain.proceed(builder.build());
             }
