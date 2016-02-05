881257ca1ab47eaf51776deb6c79c9d85451c45a
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2016-02-05 15:50:23 +0100

    Enable Picasso logging

--- app/src/main/java/fr/masciulli/drinks/DrinksApplication.java
+++ app/src/main/java/fr/masciulli/drinks/DrinksApplication.java
@@ -3,11 +3,14 @@ package fr.masciulli.drinks;
 import android.app.Application;
 
 import com.squareup.leakcanary.LeakCanary;
+import com.squareup.picasso.Picasso;
 
 public class DrinksApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
         LeakCanary.install(this);
+        Picasso picasso = Picasso.with(this);
+        picasso.setLoggingEnabled(BuildConfig.DEBUG);
     }
 }
