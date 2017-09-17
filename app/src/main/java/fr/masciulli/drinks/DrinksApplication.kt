25e2f870d90ef6537801e8cecbe22cf98cda4fa4
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2017-09-17 18:42:23 +0200

    Convert app class to Kotlin

---
+++ app/src/main/java/fr/masciulli/drinks/DrinksApplication.kt
@@ -0,0 +1,35 @@
+package fr.masciulli.drinks
+
+import android.app.Application
+import android.content.Context
+
+import com.squareup.leakcanary.LeakCanary
+
+import fr.masciulli.drinks.core.net.Client
+import fr.masciulli.drinks.core.drinks.DrinksSource
+import fr.masciulli.drinks.core.liquors.LiquorsSource
+
+class DrinksApplication : Application() {
+    lateinit var drinksSource: DrinksSource
+        private set
+    lateinit var liquorsSource: LiquorsSource
+        private set
+
+    override fun onCreate() {
+        super.onCreate()
+        if (!LeakCanary.isInAnalyzerProcess(this)) {
+            LeakCanary.install(this)
+        }
+
+        val client = Client(BuildConfig.SERVER_URL)
+        drinksSource = client
+        liquorsSource = client
+    }
+
+    companion object {
+        @JvmStatic
+        fun get(context: Context): DrinksApplication {
+            return context.applicationContext as DrinksApplication
+        }
+    }
+}
