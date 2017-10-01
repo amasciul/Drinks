6c10419b473bafb52fadab257c1b5af743d33aaa
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2017-10-01 13:45:45 +0200

    Add cache to drinks repository

--- core/src/main/java/fr/masciulli/drinks/core/drinks/DrinksRepository.kt
+++ core/src/main/java/fr/masciulli/drinks/core/drinks/DrinksRepository.kt
@@ -1,6 +1,32 @@
 package fr.masciulli.drinks.core.drinks
 
+import rx.Observable
+
 class DrinksRepository(private val remoteSource: DrinksSource) : DrinksSource {
-    override fun getDrinks() = remoteSource.getDrinks()
-    override fun getDrink(id: String) = remoteSource.getDrink(id)
+    private val cachedDrinks: MutableMap<String, Drink> = LinkedHashMap()
+    private var cached = false
+
+    override fun getDrinks(): Observable<List<Drink>> {
+        if (cached) {
+            return Observable.just(cachedDrinks.values.toList())
+        }
+        return remoteSource.getDrinks().doOnNext { cacheDrinks(it) }
+    }
+
+    override fun getDrink(id: String): Observable<Drink> {
+        val cachedDrink = cachedDrinks[id]
+        if (cachedDrink != null) {
+            return Observable.just(cachedDrink)
+        }
+        return remoteSource.getDrink(id).doOnNext { cacheDrinks(listOf(it)) }
+    }
+
+    private fun cacheDrinks(drinks: List<Drink>) {
+        cachedDrinks.clear()
+
+        drinks.forEach {
+            cachedDrinks[it.id] = it
+        }
+        cached = true
+    }
 }
