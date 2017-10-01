81058ec1612e3debe977bdcea4ffa18630a8c16a
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2017-10-01 16:19:15 +0200

    Add cacche to liquors repository

--- core/src/main/java/fr/masciulli/drinks/core/liquors/LiquorsRepository.kt
+++ core/src/main/java/fr/masciulli/drinks/core/liquors/LiquorsRepository.kt
@@ -3,6 +3,30 @@ package fr.masciulli.drinks.core.liquors
 import rx.Observable
 
 class LiquorsRepository(private val remoteSource: LiquorsSource) : LiquorsSource {
-    override fun getLiquors(): Observable<List<Liquor>> = remoteSource.getLiquors()
-    override fun getLiquor(id: String): Observable<Liquor> = remoteSource.getLiquor(id)
+    private val cachedLiquors: MutableMap<String, Liquor> = LinkedHashMap()
+    private var cached = false
+
+    override fun getLiquors(): Observable<List<Liquor>> {
+        if (cached) {
+            return Observable.just(cachedLiquors.values.toList())
+        }
+
+        return remoteSource.getLiquors().doOnNext { cacheLiquors(it) }
+    }
+
+    override fun getLiquor(id: String): Observable<Liquor> {
+        val cachedLiquor = cachedLiquors[id]
+        if (cachedLiquor != null) {
+            return Observable.just(cachedLiquor)
+        }
+
+        return remoteSource.getLiquor(id).doOnNext { cacheLiquors(listOf(it)) }
+    }
+
+    private fun cacheLiquors(liquors: List<Liquor>) {
+        liquors.forEach {
+            cachedLiquors[it.id] = it
+        }
+        cached = true
+    }
 }
